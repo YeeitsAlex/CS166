3bf1d232920a9e0834ca20393d3162d38a258011
Alexander Yee <ayee013@ucr.edu>
2019-04-22 18:14:53 -0700

    Removed the ON DELETE NO ACTIONS and fixed syntax errors

--- Lab03/LAB3_PART1.sql
+++ Lab03/LAB3_PART1.sql
@@ -8,12 +8,12 @@ DROP TABLE IF EXISTS Work_Proj
 
 CREATE TABLE Professor	(ssn CHAR(11) NOT NULL,
 			name CHAR(30) NOT NULL,
-			age INTEGER NOT NULL,
-			p_rank INTEGER NOT NULL,
+			age Integer NOT NULL,
+			p_rank Integer NOT NULL,
 			specialty CHAR(100) NOT NULL,
 			PRIMARY KEY(ssn));
 
-CREATE TABLE Project	(pno INTEGER NOT NULL,
+CREATE TABLE Project	(pno Integer NOT NULL,
 			ssn CHAR(30) NOT NULL,
  		 	sponsor CHAR(30) NOT NULL,
 			start_date DATE NOT NULL,
@@ -23,38 +23,37 @@ CREATE TABLE Project	(pno INTEGER NOT NULL,
 			FOREIGN KEY(ssn) REFERENCES Professor(ssn));
 
 CREATE TABLE Works_In	(ssn CHAR(11) NOT NULL,
-			pno INTEGER,
+			pno Integer,
 			PRIMARY KEY(ssn,pno),
-			FOREIGN KEY(ssn) REFERENCES Professor(ssn) ON DELETE NO ACTION,
-			FOREIGN KEY(pno) REFERENCES Project(pno) ON DELETE NO ACTION );
+			FOREIGN KEY(ssn) REFERENCES Professor(ssn) ,
+			FOREIGN KEY(pno) REFERENCES Project(pno)  );
 
-CREATE TABLE Dept	(dno INTEGER NOT NULL,
+CREATE TABLE Dept	(dno Integer NOT NULL,
 			ssn CHAR(30) NOT NULL,
 			dname CHAR(30) NOT NULL,
 			office CHAR(30) NOT NULL,
 			PRIMARY KEY(dno),
-			FOREIGN KEY(ssn) REFERENCES Professor(ssn) ON DELETE NO ACTION);
+			FOREIGN KEY(ssn) REFERENCES Professor(ssn) );
 
 CREATE TABLE Work_Dept	(ssn CHAR(11) NOT NULL,
-			dno INTEGER,
+			dno Integer,
 			PRIMARY KEY(ssn, dno),
-			FOREIGN KEY(ssn) REFERENCES Professor(ssn) ON DELETE NO ACTION,
-			FOREIGN KEY(dno) REFERENCES DEPT(dno) ON DELETE NO ACTION );
+			FOREIGN KEY(ssn) REFERENCES Professor(ssn) ,
+			FOREIGN KEY(dno) REFERENCES Dept(dno)  );
 
 CREATE TABLE Graduate	(ssn CHAR(11) NOT NULL,
-			advisor_ssn CHAR(11) NOT NULL,
+			advisor text NOT NULL,
 			name CHAR(30) NOT NULL,
-			age INTEGER NOT NULL,
+			age Integer NOT NULL,
 			deg_pg CHAR(30) NOT NULL,
 			PRIMARY KEY(ssn),
-			FOREIGN KEY(advisor_ssn) REFERENCES Graduate(ssn) ON DELETE NO ACTION,
-			FOREIGN KEY(dno) REFERENCES Dept(dno) ON DELETE NO ACTION );
+			FOREIGN KEY(dno) REFERENCES Dept(dno)  );
 
-CREATE TABLE Work_Proj	(pno INTEGER NOT NULL,
+CREATE TABLE Work_Proj	(pno Integer NOT NULL,
 			grad_ssn CHAR(11) NOT NULL,
 			prof_ssn CHAR(11) NOT NULL,
 			since DATE NOT NULL,
 			PRIMARY KEY(pno,grad_ssn),
-			FOREIGN KEY(pno) REFERENCES Project(pno) ON DELETE NO ACTION,
-			FOREIGN KEY(grad_ssn) REFERENCES Graduate(ssn) ON DELETE NO ACTION,
-			FOREIGN KEY(prof_ssn) REFERENCES Professor(ssn) ON DELETE NO ACTION );
+			FOREIGN KEY(pno) REFERENCES Project(pno) ,
+			FOREIGN KEY(grad_ssn) REFERENCES Graduate(ssn) ,
+			FOREIGN KEY(prof_ssn) REFERENCES Professor(ssn)  );
